ce0c0f9d332ab01227a99c4481fa005ea5026659
Timo Mämecke <timo@maemecke.com>
2018-06-05 00:32:45 +0200

    Change shortHtml to own-rendered html

--- posts/2018-06-03-test-content.md
+++ posts/2018-06-03-test-content.md
@@ -1,9 +1,7 @@
 ---
-title: "Hello was geht"
+title: Man kennt, dass ich zwischendurch einfach mal verschwinde
 path: hello-was-geht
 date: "2018-06-03"
 ---
 
-Also. Was geht hier.
-
-So ab.
+Ich arbeitete nach dem Studium zuerst freiberuflich, der Büroalltag kam erst durch Railslove. Auch ich habe häufig wenig produktive Phasen im Büro, in denen ich aber trotzdem arbeite, oft mit dem Ergebnis dass einige Stunden nicht abrechenbar sind. Die fallen natürlich immer an und das ist auch eingeplant. Die Energie fehlt mir aber fürs Arbeiten an privaten Projekten, die ich immer wieder verfolge und in denen ich mich weiterbilde.
--- src/nodes/markdown-remark.js
+++ src/nodes/markdown-remark.js
@@ -1,22 +1,26 @@
 const { GraphQLString, GraphQLBoolean } = require('graphql')
 const MORE_SEP = '<!-- more -->'
 
+const remark = require('remark')
+const remarkHtml = require('remark-html')
+const remarkFrontmatter = require('remark-frontmatter')
+
 module.exports = ({ type, store, pathPrefix, getNode, cache, reporter }) => {
   function hasMore(node) {
     return node.internal.content.includes(MORE_SEP)
   }
 
-  async function getHTML(node) {
-    return await cache.get(
-      `transformer-remark-markdown-html-${node.internal.contentDigest}--`
-    )
-  }
-
-  async function shortHtml(node) {
-    const more = hasMore(node)
-    const html = await getHTML(node)
-    const [short] = html.split(MORE_SEP)
-    return short
+  function shortHtml(node) {
+    return new Promise((resolve, reject) => {
+      const [short] = node.internal.content.split(MORE_SEP)
+      remark()
+        .use(remarkHtml)
+        .use(remarkFrontmatter, ['yaml'])
+        .process(short, (err, file) => {
+          if (err) return reject(err)
+          resolve(String(file))
+        })
+    })
   }
 
   return Promise.resolve({
